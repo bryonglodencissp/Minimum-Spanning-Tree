70e828cdc0e775136c580eaa4dad5279b3027114
bryonglodencissp <bryongloden@bryon-mac1.fios-router.home>
2019-07-20 16:45:58 -0400

    adding attribution

--- main.java
+++ main.java
@@ -138,10 +138,10 @@ class Graph
 
 		/* Let us create following weighted graph 
 				10 
-			0--------1 
-			| \	 | 
-		6| 5\ |15 
-			|	 \ | 
+			0--------1
+			| \	     | 
+		   6|   5\   |15 
+			|	   \ | 
 			2--------3 
 				4	 */
 		int V = 4; // Number of vertices in graph 
@@ -176,5 +176,5 @@ class Graph
 		graph.KruskalMST(); 
 	} 
 } 
-//This code is contributed by Aakash Hasija 
+//This code is contributed by Bryon Gloden, CISSP®
 
